19893084786857f48b3989f8cee6544705b54927
Giso Grimm <giso.grimm@vegri.net>
2024-03-25 23:24:15 +0100

    correct winsock2 initialization

--- source/zita-n2j.cc
+++ source/zita-n2j.cc
@@ -271,7 +271,21 @@ static int opensocket(Sockaddr* A)
 int main(int ac, char* av[])
 {
 #ifdef _WIN32
-    WSAStartup();
+    WORD wVersionRequested;
+    WSADATA wsaData;
+    int err;
+
+    /* Use the MAKEWORD(lowbyte, highbyte) macro declared in Windef.h */
+    wVersionRequested = MAKEWORD(2, 2);
+
+    err = WSAStartup(wVersionRequested, &wsaData);
+    if(err != 0)
+    {
+        /* Tell the user that we could not find a usable */
+        /* Winsock DLL.                                  */
+        printf("WSAStartup failed with error: %d\n", err);
+        return 1;
+    }
 #endif
     Sockaddr Arx, Atx;
     int nchan, fsamp, filt, sockfd;
